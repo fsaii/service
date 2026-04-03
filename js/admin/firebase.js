b152181546531b04d18795530dfe9fc444803a84
f.saii <f.saii@fsaii-2.local>
2026-04-03 14:21:25 +0700

    first commit

--- js/admin/firebase.js
+++ js/admin/firebase.js
@@ -2,15 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "xxxx",
-  authDomain: "xxxx",
-  projectId: "xxxx",
-  storageBucket: "xxxx",
-  messagingSenderId: "xxxx",
-  appId: "xxxx"
+  apiKey: "AIzaSyCqRDXBWRbUvzFH0M7vp-bRD0WxHX70w1Y",
+  authDomain: "air-cleaning-test.firebaseapp.com",
+  projectId: "air-cleaning-test",
+  storageBucket: "air-cleaning-test.firebasestorage.app",
+  messagingSenderId: "408424583515",
+  appId: "1:408424583515:web:db90ff6ad722afc3390a7e",
+  measurementId: "G-KYE14YHYFH"
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };1
+export const db = getFirestore(app);
